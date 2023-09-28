45c552b7792aa26a3c10f2e7f044e1fd5bb8a135
jolly6 <98voonfusing@gmail.com>
2023-09-28 08:47:17 +0800

    Fixed detailservice in router

--- pkg/api/router.go
+++ pkg/api/router.go
@@ -94,16 +94,15 @@ func (s *Server) gameService() game.Service {
 }
 
 func (s *Server) detailService() source.DetailService {
-	//services := map[string]source.DetailService{
-	//	game.YGO: detail.NewYGO(s.log, s.gameService()),
-	//}
-	//
-	//return detail.NewCachedDetailService(
-	//	s.cache,
-	//	s.cfg,
-	//	s.log,
-	//	services,
-	//	s.gameService(),
-	//)
-	return detail.NewYGO(s.log, s.gameService())
+	services := map[string]source.DetailService{
+		game.YGO: detail.NewYGO(s.log, s.gameService()),
+	}
+
+	return detail.NewCachedDetailService(
+		s.cache,
+		s.cfg,
+		s.log,
+		services,
+		s.gameService(),
+	)
 }
